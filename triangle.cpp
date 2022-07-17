9e0c66b386ab009927d1e51f9c0ac87770fedd42
董雅婷 <3046699620@qq.com>
2022-07-17 13:38:40 +0800

    倒三角

---
+++ triangle.cpp
@@ -0,0 +1,20 @@
+//倒三角
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=i;j<i+(n-i)*2-1;j++)
+        {
+           cout<<"*";
+        }
+        cout<<"\n";
+    }
+}
